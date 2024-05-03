5115f86c75be64559679dcf7414cf23a3f0a6d8c
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-05-03 19:33:36 +0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace StudentGradesApp
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "......";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectToDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            if (ConnectToDatabase())
+            {
+                MessageBox.Show("Connection successful!");
+
+                DisplayStudentGrades();
+            }
+            else
+            {
+                MessageBox.Show("Connection failed!");
+            }
+        }
+
+        private bool ConnectToDatabase()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        private void DisplayStudentGrades()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM StudentGrades";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    studentGradesListBox.ItemsSource = dataTable.DefaultView;
+                }
+            }
+        }
+    }
+}
